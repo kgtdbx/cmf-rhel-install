dc640153d3e3f535e69a57f616ca978496fa882a
Ted Coyle <tedc@DataJawn.local>
2016-05-09 19:58:35 -0400

    mysql install fix

--- 06InstallMySQL.sh
+++ 06InstallMySQL.sh
@@ -6,7 +6,13 @@ do
 
    #commands go here
       
-    ssh root@$node "wget http://repo.mysql.com//mysql57-community-release-el6-7.noarch.rpm; yum -y install mysql57-community-release-el6-7.noarch.rpm; yum-config-manager --disable mysql57-community; yum-config-manager --enable mysql56-community; yum -y install mysql-community-server; exit"
+    ssh root@$node "wget http://repo.mysql.com//mysql57-community-release-el6-7.noarch.rpm; yum -y install mysql57-community-release-el6-7.noarch.rpm; yum-config-manager --disable mysql57-community; yum-config-manager --enable mysql56-community; yum -y install mysql-community-server; chkconfig --add mysqld; service mysqld stop; exit"
+	
+	scp root@$node primary-my.cnf /etc/my.cnf
+	
+	ssh root@$node "service mysqld start"
+	
+	
     echo END $node
 
 done < nodes
