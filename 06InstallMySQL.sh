5df09977573eed44ecd7e96e2437c6c758b29d77
Ted Coyle <tedc@DataJawn.local>
2016-05-09 21:20:52 -0400

    mysql install refined - add removemysql.sh

--- 06InstallMySQL.sh
+++ 06InstallMySQL.sh
@@ -6,11 +6,18 @@ do
 
    #commands go here
       
-    ssh root@$node "wget http://repo.mysql.com//mysql57-community-release-el6-7.noarch.rpm; yum -y install mysql57-community-release-el6-7.noarch.rpm; yum-config-manager --disable mysql57-community; yum-config-manager --enable mysql56-community; yum -y install mysql-community-server; chkconfig --add mysqld; service mysqld stop; exit"
+    ssh root@$node "wget http://repo.mysql.com//mysql57-community-release-el6-7.noarch.rpm;exit;"
+    ssh root@$node "yum -y install mysql57-community-release-el6-7.noarch.rpm;exit;"
+    ssh root@$node "yum-config-manager --disable mysql57-community;exit;"
+    ssh root@$node "yum-config-manager --enable mysql56-community;exit;"
+    ssh root@$node "yum -y install mysql-community-server;exit;"
+    ssh root@$node "chkconfig --add mysqld; service mysqld stop; exit;"
 	
-	scp root@$node primary-my.cnf /etc/my.cnf
+    scp primary-my.cnf root@$node:/etc/my.cnf
 	
-	ssh root@$node "service mysqld start"
+    ssh root@$node "service mysqld start;exit;"
+    ssh root@$node "/usr/bin/mysqladmin -u root password 'cloudera';exit;"
+    ssh root@$node "/usr/bin/mysqladmin -u root -h localhost.localdomain password 'cloudera'"
 	
 	
     echo END $node
