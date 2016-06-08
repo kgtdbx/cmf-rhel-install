75f5d68d590b4c68e8776dc94d98661d961c738f
Ted Coyle <tedc@DataJawn.local>
2016-06-07 21:48:54 -0400

    mysql install updated

--- 06InstallMySQL.sh
+++ 06InstallMySQL.sh
@@ -8,17 +8,17 @@ do
 
    #commands go here
       
-      ssh $LOGIN@$node "wget http://repo.mysql.com//mysql57-community-release-el6-7.noarch.rpm;exit;"
-      ssh $LOGIN@$node "yum -y install mysql57-community-release-el6-7.noarch.rpm;exit;"
-      ssh $LOGIN@$node "yum-config-manager --disable mysql57-community;exit;"
-      ssh $LOGIN@$node "yum-config-manager --enable mysql56-community;exit;"
-      ssh $LOGIN@$node "yum -y install mysql-community-server;exit;"
-      ssh $LOGIN@$node "chkconfig --add mysqld; service mysqld stop; exit;"
+      echo begin| ssh $LOGIN@$node "wget http://repo.mysql.com//mysql57-community-release-el6-7.noarch.rpm;exit;"
+      echo begin| ssh $LOGIN@$node "yum -y install mysql57-community-release-el6-7.noarch.rpm;exit;"
+      echo begin| ssh $LOGIN@$node "yum-config-manager --disable mysql57-community;exit;"
+      echo begin| ssh $LOGIN@$node "yum-config-manager --enable mysql56-community;exit;"
+      echo begin| ssh $LOGIN@$node "yum -y install mysql-community-server;exit;"
+      echo begin| ssh $LOGIN@$node "chkconfig --add mysqld; service mysqld stop; exit;"
 	
       scp primary-my.cnf $LOGIN@$node:/etc/my.cnf
 	
-      ssh $LOGIN@$node "service mysqld start;exit;"
-      ssh $LOGIN@$node "/usr/bin/mysqladmin -u root password 'cloudera';exit;"
+      echo begin| ssh $LOGIN@$node "service mysqld start;exit;"
+      echo begin| ssh $LOGIN@$node "/usr/bin/mysqladmin -u root password 'cloudera';exit;"
 	
    echo END $node
 
